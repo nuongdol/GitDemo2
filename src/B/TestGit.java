585ef74b9555e6d425e47f59eb93fbb471c92f61
nuong.dt <dangthinuong1804@gmail.com>
2024-10-29 23:02:48 +0700

    Description: Add more more

--- src/B/TestGit.java
+++ src/B/TestGit.java
@@ -3,5 +3,6 @@ package B;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello nuong, how are you. I am from Nam Dinh.");
+        System.out.println("Tai sao t cu nhu vay? t cung khong biet nua.");
     }
 }
