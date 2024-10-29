491da8c143c1d28358ddbb43820b0551eaca55ce
nuong.dt <dangthinuong1804@gmail.com>
2024-10-29 23:50:23 +0700

    Description: comment

--- src/B/BadSales.java
+++ src/B/BadSales.java
@@ -3,6 +3,7 @@ package B;
 public class BadSales {
     public static void main(String[] args) {
         System.out.println("toi nho nha qua uoc gi duoc ve.");
+        //comment de tam day nha
     }
 }
 
