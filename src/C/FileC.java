10309e7acb3245ee2f5c6e64144db2d9368208a7
nuong.dt <dangthinuong1804@gmail.com>
2024-10-29 23:44:50 +0700

    Description: uoc vua thoi, nuong ham

---
+++ src/C/FileC.java
@@ -0,0 +1,8 @@
+package C;
+
+public class FileC {
+    public static void main(String[] args) {
+        System.out.println("uoc gi minh la con ngua, de di nhu bay");
+        System.out.println("uoc gi minh la bong hoa, de toa huong thom cho doi.");
+    }
+}
