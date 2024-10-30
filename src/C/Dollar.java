333f96f93065ddc31b3dcc293ca52cb7b6928292
nuong.dt <dangthinuong1804@gmail.com>
2024-10-30 08:35:13 +0700

    Description: Co len anh em

--- src/C/Dollar.java
+++ src/C/Dollar.java
@@ -3,6 +3,7 @@ package C;
 public class Dollar {
     public static void main(String[] args) {
         System.out.println("happy birthday nuong dol");
+        System.out.println("Co len anh em, can gi cu bao nhau nha.");
     }
 
 }
