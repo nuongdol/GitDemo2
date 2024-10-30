93b904804a438a144e5aac2ef6cfe0e302e5d4c6
nuong.dt <dangthinuong1804@gmail.com>
2024-10-30 13:37:42 +0700

    Description: Debug endless point

---
+++ src/Debug/Endless.java
@@ -0,0 +1,25 @@
+package Debug;
+
+import java.io.IOException;
+
+public class Endless {
+    public static void main(String[] args) throws IOException {
+        while(true){
+            int read = System.in.read();
+            System.out.println("Input " + read);
+            if(filter(read)){
+                process(read);
+            }
+        }
+    }
+    private static boolean filter(int read){
+        return read != '\n' && read != 'a';
+    }
+    private static void process(int arg){
+        if(Math.max(arg, 90) % 2 == 0){
+            System.out.println("!");
+            int a = arg / 7;
+            System.out.println(a);
+        }
+    }
+}
