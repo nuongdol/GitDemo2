12c991ed7a29b5a4a1194da9712c051ac1f71e1c
nuong.dt <dangthinuong1804@gmail.com>
2024-11-04 10:51:07 +0700

    Description: Update Dollar

--- src/C/Dollar.java
+++ src/C/Dollar.java
@@ -4,6 +4,7 @@ public class Dollar {
     public static void main(String[] args) {
         System.out.println("happy birthday nuong dol");
         System.out.println("Co len anh em, can gi cu bao nhau nha.");
+        System.out.println("Lam lai git mot lan nua nao, gang len.");
     }
 
 }
