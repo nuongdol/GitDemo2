773e05fa0c01184484a3813c7822088627b378bf
nuong.dt <dangthinuong1804@gmail.com>
2024-11-08 21:47:27 +0700

    Description: Add file Traveral

--- src/C/Traveral.java
+++ src/C/Traveral.java
@@ -3,4 +3,8 @@ package C;
 public class Traveral {
     //di du lich vòng quanh the gioi la uoc mo cua toi
     //lam gi di chu cu vay mai a
+    /*
+    toi chan vi toi khong lam duoc gi ca, chan lam, phai lam sao phai lam sao day
+    lieu toi co lam duoc khong vi da 2 thang hon r, gio toi phai lam the nao.
+     */
 }
